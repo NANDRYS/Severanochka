47dff774c86db72d628e2b2bc07a9fc22e792acc
Nesterov Nikita <nik-ta@mail.ru>
2025-03-26 20:39:54 +0300

    modified js

--- BEM/5/script.js
+++ BEM/5/script.js
@@ -1,15 +1,15 @@
-const counter = document.querySelector(".counter");
-const counterNum = document.querySelector(".counter__num");
-let curentValue = parseInt(counterNum.textContent, 10);
+document.addEventListener('click', (e) => {
+  if (e.target.matches('.counter__btn-plus') || e.target.matches('.counter__btn-minus')) {
+    const counter = e.target.closest('.counter');
+    const counterNum = counter.querySelector('.counter__num');
+    let currentValue = parseInt(counterNum.textContent, 10);
 
-counter.addEventListener("click", (e) => {
-  if (e.target.matches(".counter__btn-plus")) {
-    curentValue += 1;
-    counterNum.textContent = curentValue;
-  } else if (e.target.matches(".counter__btn-minus")) {
-    if (curentValue > 0) {
-      curentValue -= 1;
+    if (e.target.matches('.counter__btn-plus')) {
+      currentValue += 1;
+    } else if (e.target.matches('.counter__btn-minus') && currentValue > 0) {
+      currentValue -= 1;
     }
-    counterNum.textContent = curentValue;
+
+    counterNum.textContent = currentValue;
   }
 });
