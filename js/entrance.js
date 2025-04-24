b7a3ef084a13f721e65bd5bce9ab61dbc63c0446
max28p <maxim131006@gmail.com>
2025-04-24 22:41:11 +0300

    feature/section-8

---
+++ js/entrance.js
@@ -0,0 +1,9 @@
+function entrance() {
+    const open = document.getElementById("section8Open")
+    open.classList.add("section-8_open")
+}
+
+function closeSection() {
+    const close = document.getElementById("section8Open")
+    close.classList.remove("section-8_open")
+}
